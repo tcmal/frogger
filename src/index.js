a8886b085334e0de2890feb9ba4bbe0d094cda73
mal <oscar.shrimpton.personal@gmail.com>
2020-04-03 15:52:20 +0100

    React Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import DevTools from "mobx-react-devtools";
 
 import TodoList from "./components/TodoList";
@@ -11,7 +12,22 @@ const store = new TodoListModel();
 render(
 	<div>
 		<DevTools />
-		<TodoList store={store} />
+		<Router>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/second">Second</Link></li>
+			</ul>
+
+			<Switch>
+				<Route path="/second">
+					<h1>It Works!</h1>
+				</Route>
+
+				<Route path="/">
+					<TodoList store={store} />
+				</Route>
+			</Switch>
+		</Router>
 	</div>,
 	document.getElementById("root")
 );
