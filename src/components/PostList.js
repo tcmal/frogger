6fed09a725a0d4c0ab18db15d80f28cfbf02e0c8
mal <oscar.shrimpton.personal@gmail.com>
2020-04-04 16:27:54 +0100

    Add some links to PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import { friendlyTimeSince } from '../util';
+import { Link } from 'react-router-dom';
 
 export const PostListItem = ({ item }) => (
 	<div className="post">
-		<h3 className="title">{item.title}</h3>
-		<span className="poster">{item.poster_name}</span>
-		<span className="sub">{item.posted_to}</span>
+		<Link to={"/p/" + item.id} className="supressLinkStyling">
+			<h3 className="title">{item.title}</h3>
+		</Link>
+		<span className="time">{friendlyTimeSince(item.created_at)}</span>
+		<span className="poster">
+			<Link to={"/u/" + item.poster_name}>
+				/u/{item.poster_name}
+			</Link>
+		</span>
+		<span className="sub">
+			<Link to={"/f/" + item.posted_to}>
+				/f/{item.posted_to}
+			</Link>
+		</span>
 	</div>
 );
 
@@ -15,7 +28,7 @@ export default observer(({ onNextPage, onPrevPage, contents, hasPrev }) => (
 			<PostListItem key={x.id} item={x} />
 		)}
 
-		{hasPrev ? <button onClick={onPrevPage} className="btn">Previous</button> : ''}
-		<button onClick={onNextPage} className="btn">Next</button>
+		{hasPrev ? <button onClick={onPrevPage} className="btn prev">Previous</button> : ''}
+		<button onClick={onNextPage} className="btn next">Next</button>
 	</section>
 ));
